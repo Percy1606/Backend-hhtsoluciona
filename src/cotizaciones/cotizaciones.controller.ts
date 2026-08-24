e255b1a8e6defed769060e2688091d45c241b693
Percy1606 <percy@hhtsoluciona.com>
2026-08-24 16:58:14 -0500

    Feat: Directorio de uploads automatico en cotizaciones.controller.ts

--- src/cotizaciones/cotizaciones.controller.ts
+++ src/cotizaciones/cotizaciones.controller.ts
@@ -47,7 +47,13 @@ export class CotizacionesController {
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: './uploads/cotizaciones',
+        destination: (req, file, cb) => {
+          const uploadPath = './uploads/cotizaciones';
+          if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+          }
+          cb(null, uploadPath);
+        },
         filename: (req: any, file: any, cb: any) => {
           const uniqueSuffix =
             Date.now() + '-' + Math.round(Math.random() * 1e9);
